808cbb7d6720a317d2a07e245619c845d7aaf4b7
Diogo Marciano <diogomarciano40@gmail.com>
2025-11-14 19:56:40 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -13,7 +13,7 @@ import { useAuth, useFirestore } from '@/firebase';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
 import { Loader2 } from 'lucide-react';
